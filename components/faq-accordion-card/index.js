ec1bbd0cf512e84b58ab6f681e6136899c6bfb2b
devways <thomas.loyan.dev@gmail.com>
2020-11-22 14:48:47 +0100

    quick fix before pushing it in frontendmentor solution. Not totally finish and fix but I do it later, adjust css unit and try to use Framer motion

--- components/faq-accordion-card/index.js
+++ components/faq-accordion-card/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import QuestionAnswer from './QuestionAnswer';
 
 import style from './style.module.css';
@@ -8,31 +9,33 @@ const data = [
   { question: 'How do I reset my password?', answer: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.' },
   { question: 'Can I cancel my subscription?', answer: 'Yes! Send us a message and we’ll process your request no questions asked.' },
   { question: 'Do you provide additional support?', answer: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.' },
-
 ]; 
 
-const FaqAccordionCardComponent = () => (
-  <div className={style.container}>
-    <div className={style.center}>
-      <div className={style.svg_container}>
-        <div className={style.overflow}>
-          <img className={style.image_desktop} src="/faq-accordion-card/illustration-woman-online-desktop.svg" />
+const FaqAccordionCardComponent = () => {
+  const [questionKey, setQuestionKey] = useState('');
+  return (
+    <div className={style.container}>
+      <div className={style.center}>
+        <div className={style.svg_container}>
+          <div className={style.overflow}>
+            <img className={style.image_desktop} src="/faq-accordion-card/illustration-woman-online-desktop.svg" />
+          </div>
+          <img className={style.image_desktop_box} src="/faq-accordion-card/illustration-box-desktop.svg" />
+          <img className={style.image_mobile} src="/faq-accordion-card/illustration-woman-online-mobile.svg" />
         </div>
-        <img className={style.image_desktop_box} src="/faq-accordion-card/illustration-box-desktop.svg" />
-        <img className={style.image_mobile} src="/faq-accordion-card/illustration-woman-online-mobile.svg" />
-      </div>
-      <div className={style.faq_container}>
-        <div className={style.faq_subcontainer}>
-          <h2>FAQ</h2>
-          {
-            data.map(({ question, answer }) => (
-              <QuestionAnswer key={question} question={question} answer={answer} />
-            ))
-          }
+        <div className={style.faq_container}>
+          <div className={style.faq_subcontainer}>
+            <h2>FAQ</h2>
+            {
+              data.map(({ question, answer }) => (
+                <QuestionAnswer key={question} question={question} answer={answer} handleClickQuestion={() => setQuestionKey(question)} questionKey={questionKey} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default FaqAccordionCardComponent;
