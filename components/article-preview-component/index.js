9919f0d74ed310fd34f00c5d8b69ff9f0d58e47f
devways <thomas.loyan.dev@gmail.com>
2020-11-24 20:46:38 +0100

    update html and add css for mobile

--- components/article-preview-component/index.js
+++ components/article-preview-component/index.js
@@ -1,45 +1,60 @@
+import { useState } from 'react';
+import ShareLogo from '../../public/article-preview-component/icon-share.svg';
+
 import style from './style.module.css';
 
-const ArticlePreviewComponentComponent = () => (
-  <div className={style.container}>
-    <div className={style.article_preview_container}>
-      <div><img src="/article-preview-component/drawers.jpg" /></div>
-      <div>
-        <p>
-          Shift the overall look and feel by adding these wonderful 
-          touches to furniture in your home
-        </p>
-        <p>
-          Ever been in a room and felt like something was missing? Perhaps 
-          it felt slightly bare and uninviting. I’ve got some simple tips 
-          to help you make any room feel complete.
-        </p>
-        <div>
-          <div>
-            <div>
-              <div>
-                <img src="/article-preview-component/avatar-michelle.jpg" />
-              </div>
-              <div>
-                <p>Michelle Appleton</p>
-                <p>28 Jun 2020</p>
-              </div>
-            </div>
-            <div><img src="/article-preview-component/icon-share.svg" /></div>
-          </div>
-          <div>
-            <p>Share</p>
-            <div>
-              <div><img src="/article-preview-component/icon-facebook.svg" /></div>
-              <div><img src="/article-preview-component/icon-twitter.svg" /></div>
-              <div><img src="/article-preview-component/icon-pinterest.svg" /></div>
-            </div>
-            <div><img src="/article-preview-component/icon-share.svg" /></div>
-          </div>
+const ArticlePreviewComponentComponent = () => {
+  const [active, setActive] = useState(false);
+  const sharecontentstyle = active ? style.share_content + ' ' + style.active : style.share_content;
+  return (
+    <div className={style.container}>
+      <div className={style.article_preview_container}>
+        <div className={style.article_preview_image_wrapper}>
+          <img src="/article-preview-component/drawers.jpg" />
         </div>
+        <div className={style.article_preview_content}>
+          <p className={style.article_preview_content_title}>
+            Shift the overall look and feel by adding these wonderful 
+            touches to furniture in your home
+          </p>
+          <p  className={style.article_preview_content_description}>
+            Ever been in a room and felt like something was missing? Perhaps 
+            it felt slightly bare and uninviting. I’ve got some simple tips 
+            to help you make any room feel complete.
+          </p>
+        </div>
+        <div className={style.article_preview_content_author}>
+            <div className={style.author_content}>
+              <div className={style.author_content_description}>
+                <div className={style.author_content_description_image}>
+                  <img src="/article-preview-component/avatar-michelle.jpg" />
+                </div>
+                <div className={style.author_content_description_namedate}>
+                  <p>Michelle Appleton</p>
+                  <p>28 Jun 2020</p>
+                </div>
+              </div>
+              <button className={style.author_content_description_share_button} onClick={() => setActive(true)}>
+                <ShareLogo />
+              </button>
+            </div>
+            <div className={sharecontentstyle}>
+              <div className={style.share_content_main}>
+                <p>Share</p>
+                <div className={style.share_content_main_icon_container}>
+                  <div><img src="/article-preview-component/icon-facebook.svg" /></div>
+                  <div><img src="/article-preview-component/icon-twitter.svg" /></div>
+                  <div><img src="/article-preview-component/icon-pinterest.svg" /></div>
+                </div>
+              </div>
+              <button className={style.share_content_button} onClick={() => setActive(false)}>
+                <ShareLogo />
+              </button>
+            </div>
+          </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default ArticlePreviewComponentComponent;
--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
