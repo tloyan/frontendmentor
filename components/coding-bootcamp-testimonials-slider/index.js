3405096345f510d7cd1820b6cc52e870d2888fa4
devways <thomas.loyan.dev@gmail.com>
2021-01-10 20:27:12 +0100

    update html and css

--- components/coding-bootcamp-testimonials-slider/index.js
+++ components/coding-bootcamp-testimonials-slider/index.js
@@ -1,35 +1,55 @@
+import React, { useState } from 'react';
+
 import IconNext from '../../public/coding-bootcamp-testimonials-slider/icon-next.svg';
 import IconPrev from '../../public/coding-bootcamp-testimonials-slider/icon-prev.svg';
 
 import style from './style.module.css';
 
-const CodingBootcampTestimonialsSliderComponent = () => (
-  <div className={style.container}>
-    <div className={style.slider}>
-      <div className={style.slider_images}>
-        <img src="/coding-bootcamp-testimonials-slider/image-tanya.jpg" />
-      </div>
-      <div className={style.slider_buttons}>
-        <button>
-          <IconPrev />
-        </button>
-        <button>
-          <IconNext />
-        </button>
+import data from './data';
+
+const CodingBootcampTestimonialsSliderComponent = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleClickPrev = () => {
+    if (index === 0) return setIndex(data.length - 1);
+    setIndex(index - 1);
+  }
+
+  const handleClickNext = () => {
+    if(index === data.length - 1) return setIndex(0);
+    setIndex(index + 1);
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.testimonial_container}>
+        <div className={style.slider}>
+          <div className={style.slider_images}>
+            <div className={style.image_wrapper}>
+              <img src={`/coding-bootcamp-testimonials-slider/${data[index].picture}`} />
+            </div>
+          </div>
+          <div className={style.slider_buttons}>
+            <button onClick={() => handleClickPrev()}>
+              <IconPrev />
+            </button>
+            <button onClick={() => handleClickNext()}>
+              <IconNext />
+            </button>
+          </div>
+        </div>
+        <div className={style.testimonial}>
+          <p className={style.testimonial_content}>
+            “ {`${data[index].testimonial}`} ”
+          </p>
+          <div className={style.testimonial_author}>
+            <p className={style.testimonial_author_name}>{`${data[index].name}`}</p>
+            <p className={style.testimonial_author_job}>{`${data[index].job}`}</p>
+          </div>
+        </div>
       </div>
     </div>
-    <div className={style.testimonial}>
-      <p className={style.testimonial_content}>
-        “ I’ve been interested in coding for a while but never taken the jump, until now. 
-        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-        excited about the future. ”
-      </p>
-      <div className={style.testimonial_author}>
-        <p className={style.testimonial_author_name}>Tanya Sinclair</p>
-        <p className={style.testimonial_author_job}>UX Engineer</p>
-      </div>
-    </div>
-  </div>
-)
+  );
+}
 
 export default CodingBootcampTestimonialsSliderComponent;
