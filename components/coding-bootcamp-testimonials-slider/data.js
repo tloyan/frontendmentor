8e64ec8c386899747684ea31576b561923be646c
devways <thomas.loyan.dev@gmail.com>
2021-01-10 20:26:05 +0100

    create file containning data

---
+++ components/coding-bootcamp-testimonials-slider/data.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'Tanya Sinclair',
+    job: 'UX Engineer',
+    testimonial: 'I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.',
+    picture: 'image-tanya.jpg'
+  },
+  {
+    name: 'John Tarkpor',
+    job: 'Junior Front-end Developer',
+    testimonial: 'If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.',
+    picture: 'image-john.jpg'
+  }
+];
