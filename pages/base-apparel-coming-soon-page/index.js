1b6768be4724604d9cf0abce7dec47cac8ba9c3c
devways <thomas.loyan.dev@gmail.com>
2021-01-16 14:30:38 +0100

    commit base-apparel-coming-soon

---
+++ pages/base-apparel-coming-soon-page/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import BaseApparelComingSoonPageComponent from '../../components/base-apparel-coming-soon-page';
+
+const BaseApparelComingSoonPagePage = () => (
+  <Fragment>
+    <Head>
+      <title>Frontend Mentor | Base Apparel coming soon page</title>
+      <link rel="icon" href="/favicon.ico"></link>
+    </Head>
+    <BaseApparelComingSoonPageComponent />
+  </Fragment>
+);
+
+export default BaseApparelComingSoonPagePage;
