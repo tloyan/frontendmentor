96bbc140ccbfa70e20edcad156237dfce360141b
devways <thomas.loyan.dev@gmail.com>
2021-01-18 00:48:21 +0100

    add font

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,500;0,600;0,700;1,500&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet" />
           <style>{`
             #__next { height: 100% }
           `}
