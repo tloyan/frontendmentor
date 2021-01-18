537f4fdd5ad3ded93ffeb3514a21d7c87bce54fa
devways <thomas.loyan.dev@gmail.com>
2021-01-18 23:04:16 +0100

    add font and assets

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,500;0,600;0,700;1,500&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,300;0,600;0,700;1,300&display=swap" rel="stylesheet" />
           <style>{`
             #__next { height: 100% }
           `}
